ecefaace2a26fe6a8bb45ee44dfbd14219d871d3
Costa Kobrinsky <costakob@gmail.com>
2022-08-29 10:21:20 +0300

    lesson 21 Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,9 +3,31 @@ import styles from "./Dialogs.module.css"
 
 const Dialogs = (props) => {
   return (
-    <div>
-      Dialogs
+    <div className={styles.dialogsWrapper}>
+      <div className={styles.dialogsItems}>
+        <div className={styles.dialog}>
+          Costa
+        </div>
+        <div className={styles.dialog + " " + styles.active}>
+          Darya
+        </div>
+        <div className={styles.dialog}>
+          Diana
+        </div>
+        <div className={styles.dialog}>
+          Yuli
+        </div>
+        <div className={styles.dialog}>
+          Natasha
+        </div>
+      </div>
+      <div className={styles.messages}>
+        <div className={styles.message}>Hi, what are you doing today?</div>
+        <div className={styles.message}>How Are you?</div>
+        <div className={styles.message}>Yo!</div>
+      </div>
     </div>
+
   );
 }
 
